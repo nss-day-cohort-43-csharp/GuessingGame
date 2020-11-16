9cfcf9e438895ea4c5c8ec0376d00dc8c01ccb4d
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-11-16 15:08:11 -0600

    random secret number and set difficulty

--- Program.cs
+++ Program.cs
@@ -6,13 +6,14 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-
-            int secretNumber = 42;
+            Random r = new Random();
+            int secretNumber = r.Next(1, 101);
             int numberOfAttempts = 0;
+            int attemptsAllowed = GetNumberOfAttempts();
 
-            while (numberOfAttempts < 4)
+            while (numberOfAttempts < attemptsAllowed)
             {
-                Console.Write("Guess a friggen number: ");
+                Console.Write($"Guess a friggen number ({numberOfAttempts + 1}): ");
                 string userResponse = Console.ReadLine();
                 int guess = int.Parse(userResponse);
 
@@ -24,9 +25,43 @@ namespace GuessingGame
                 else
                 {
                     Console.WriteLine("Nope that wasn't it...");
+                    if (guess > secretNumber)
+                    {
+                        Console.WriteLine("Your guess was too high");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your guess was too low");
+                    }
                     numberOfAttempts++;
                 }
             }
+        }
+
+        static int GetNumberOfAttempts()
+        {
+            int numberOfAttempts = 0;
+            while (numberOfAttempts == 0)
+            {
+                Console.Write("What difficulty setting? (easy, medium, hard): ");
+                string userResponse = Console.ReadLine().ToLower();
+
+                switch (userResponse)
+                {
+                    case ("easy"):
+                        numberOfAttempts = 8;
+                        break;
+                    case ("medium"):
+                        numberOfAttempts = 6;
+                        break;
+                    case ("hard"):
+                        numberOfAttempts = 4;
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return numberOfAttempts;
         }
     }
 }
